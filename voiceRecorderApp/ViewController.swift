f71817d6190a43cfad04fbde5802c8ebad8c7830
Yaremith  Damian <laudamian27@gmail.com>
2020-02-01 11:17:15 -0600

    Initial Commit

---
+++ voiceRecorderApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  voiceRecorderApp
+//
+//  Created by Laura Yaremith Damian  Padilla on 01/02/20.
+//  Copyright © 2020 Laura Yaremith Damian  Padilla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ voiceRecorderAppTests/voiceRecorderAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  voiceRecorderAppTests.swift
+//  voiceRecorderAppTests
+//
+//  Created by Laura Yaremith Damian  Padilla on 01/02/20.
+//  Copyright © 2020 Laura Yaremith Damian  Padilla. All rights reserved.
+//
+
+import XCTest
+@testable import voiceRecorderApp
+
+class voiceRecorderAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
